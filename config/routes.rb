7c495f47ce002a10d5077314e434f6ac4b6a454e
denniskamau24 <denniskangethek@gmail.com>
2023-07-04 16:53:21 +0100

    done

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  
+  resources :foods
+  resources :orderitems
+  resources :orders
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :recipes, only:[:index] do 
+  resources :users
+end
+resources :recipes, only:[:index,:create]
+
+post "/signup", to: 'users#create'
+get "/me", to:'users#show'
+post "/login", to: 'sessions#create'
+delete '/logout', to:'sessions#destroy'
+end
